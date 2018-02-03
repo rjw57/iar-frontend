527b10f506df100d4480e4e7d7ae031464f64fcc
Rich Wareham <rich@richwareham.com>
2018-02-03 18:40:36 +0000

    add "zero view" for when there are no assets in the result
    Add a special ZeroAssetsRow component which is used to indicate that no
    assets are in the returned query. This is only displayed if a load is
    not currently in progress to avoid a flash of content.

--- src/components/AssetTable.js
+++ src/components/AssetTable.js
@@ -6,14 +6,17 @@ import {
   TableHeader,
   TableHeaderColumn,
   TableRow,
+  TableRowColumn,
 } from 'material-ui/Table';
 import AssetListItem from '../components/AssetListItem';
 import { connect } from 'react-redux';
 
 /**
- * A table showing the assets currently in the asset summary table. 
+ * A table showing the assets currently in the asset summary table.
+ *
+ * The unconnected component is exported by name.
  */
-const AssetTable = ({ assetSummaries }) => (
+export const AssetTable = ({ assetSummaries, isLoadingAssets = false }) => (
   <div className="Asset-table">
     <Table
       fixedHeader={true}
@@ -37,6 +40,10 @@ const AssetTable = ({ assetSummaries }) => (
         displayRowCheckbox={false}
         className="Asset-table-body"
       >
+        {
+          // Display a "no assets" row if there is no loading happening and there are no assets.
+          ((assetSummaries.length === 0) && !isLoadingAssets) ? <ZeroAssetsRow /> : null
+        }
         { assetSummaries.map(asset => <AssetListItem key={asset.url} assetUrl={asset.url} />) }
       </TableBody>
     </Table>
@@ -45,10 +52,23 @@ const AssetTable = ({ assetSummaries }) => (
 
 AssetTable.propTypes = {
   assetSummaries: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isLoadingAssets: PropTypes.bool,
 };
+
+/**
+ * Table row which is displayed when there are no assets in the current list.
+ */
+export const ZeroAssetsRow = () => (
+  <TableRow>
+    <TableRowColumn colSpan={6} style={{textAlign: 'center'}}>
+      There are no assets to display
+    </TableRowColumn>
+  </TableRow>
+);
 
 const mapStateToProps = ({ iarApi }) => ({
   assetSummaries: iarApi.assets.summaries,
+  isLoadingAssets: iarApi.assets.isLoading,
 });
 
 export default connect(mapStateToProps)(AssetTable);
---
+++ src/components/AssetTable.test.js
@@ -0,0 +1,36 @@
+import '../test/mocks';
+
+import React from 'react';
+
+// Provide a mock AssetListItem implementation since we only care that it has the right props set.
+jest.mock('./AssetListItem', () => () => <div />);
+
+import TestRenderer from 'react-test-renderer';
+import { AssetTable, ZeroAssetsRow } from './AssetTable';
+import AssetListItem from './AssetListItem';
+import { render } from '../testutils';
+
+test('AssetTable contains no AssetListItem if no assets', () => {
+  const instance = render(<AssetTable assetSummaries={[]} />);
+  expect(instance.findAllByType(AssetListItem)).toEqual([]);
+});
+
+test('AssetTable contains a ZeroAssetsRow if no assets', () => {
+  const instance = render(<AssetTable assetSummaries={[]} />);
+  instance.findByType(ZeroAssetsRow);
+});
+
+test('AssetTable does not contain a ZeroAssetsRow if no assets but we are loading', () => {
+  const instance = render(<AssetTable isLoadingAssets={true} assetSummaries={[]} />);
+  expect(instance.findAllByType(ZeroAssetsRow)).toEqual([]);
+});
+
+test('AssetTable contains an AssetListItem for each asset summary', () => {
+  const summaries = [{url: 'a'}, {url: 'b'}];
+  const instance = render(<AssetTable assetSummaries={summaries} />);
+  const items = instance.findAllByType(AssetListItem);
+  expect(items.length).toBe(2);
+  summaries.forEach((asset, index) => {
+    expect(items[index].props).toMatchObject({ assetUrl: asset.url });
+  });
+});
